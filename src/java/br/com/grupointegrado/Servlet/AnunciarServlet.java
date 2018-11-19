16fec2b7daa11588a138705fba7ec88b498d0fcc
Eglison <eglison.souza@grupointegrado.br>
2018-11-19 12:27:48 -0200

    exemplo de requisição de formulario post

--- src/java/br/com/grupointegrado/Servlet/AnunciarServlet.java
+++ src/java/br/com/grupointegrado/Servlet/AnunciarServlet.java
@@ -6,7 +6,6 @@
 package br.com.grupointegrado.Servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -21,10 +20,28 @@ public class AnunciarServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-       
+
         RequestDispatcher JotaQuest = req.getRequestDispatcher("/WEB-INF/paginas/anunciar.jsp");
         JotaQuest.forward(req, resp);
-       
+
     }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            ProdutoModel produto = new ProdutoModel();
+            ProdutoController controller = new ProdutoController();
+            produto.setDescProduto(request.getParameter("descricao"));
+            produto.setDescMarca(request.getParameter("marca"));
+            produto.setQntProduto(Integer.parseInt(request.getParameter("qntidade")));
+            produto.setValorUnitario(Float.parseFloat(request.getParameter("valorUnitario").replace(",", ".")));
+            controller.salvar(produto);
+            response.sendRedirect("/admin/SalvoSucesso.html");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    ''
 
 }
